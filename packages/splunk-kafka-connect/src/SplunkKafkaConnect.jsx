d2f36bb818dad51ea94d4ad896c5fc807f08d8fd
Sharon Xie <rxie@splunk.com>
2017-10-10 21:51:22 -0700

    allow pause/resume/restart/delete connectors

--- packages/splunk-kafka-connect/src/SplunkKafkaConnect.jsx
+++ packages/splunk-kafka-connect/src/SplunkKafkaConnect.jsx
@@ -8,51 +8,56 @@ import NewConnectorModal from './components/NewConnectorModal'
 class SplunkKafkaConnect extends Component {
     constructor(props) {
         super(props);
+        this.state = { connectors: [], open: false, plugins: [] };
+        
+        this.getConnectors();
+    }
+
+    getConnectors = () => {
         const connectors = [];
-        this.state = { connectors, open: false, plugins: [] };
         const connectorsUrl = urls.baseUrl + urls.connectors;
         fetch(connectorsUrl)
-            .then(res => {
-                if (res.ok) {
-                    return res.json();
-                }
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            }
 
-                throw new Error('Cannot fetch connectors');
-            }).then(res => {
-                const promises = [];
+            throw new Error('Cannot fetch connectors');
+        }).then(res => {
+            const promises = [];
 
-                _.each(res, (connector) => {
-                    promises.push(fetch(`${connectorsUrl}/${connector}/status`));
-                })
-
-                return Promise.all(promises);
-            }).then(promises => {
-                const resArray = [];
-                _.each(promises, promise=> {
-                    resArray.push(promise.json());
-                });
-
-                return Promise.all(resArray);
-            }).then(jsonArray => {
-                _.each(jsonArray, data => {
-                    const connector = {};
-                    connector.name = data.name;
-                    connector.status = data.connector.state;
-                    connector.numTasks = data.tasks.length;
-                    connectors.push(connector);
-                });
-
-                this.setState({connectors});
-            }).catch(err => {
-                console.error(`Problems with fetching connectors ${err.message}`);
+            _.each(res, (connector) => {
+                promises.push(fetch(`${connectorsUrl}/${connector}/status`));
             })
+
+            return Promise.all(promises);
+        }).then(promises => {
+            const resArray = [];
+            _.each(promises, promise=> {
+                resArray.push(promise.json());
+            });
+
+            return Promise.all(resArray);
+        }).then(jsonArray => {
+            _.each(jsonArray, data => {
+                const connector = {};
+                connector.name = data.name;
+                connector.status = data.connector.state;
+                connector.numTasks = data.tasks.length;
+                connectors.push(connector);
+            });
+
+            this.setState({connectors});
+        }).catch(err => {
+            console.error(`Problems with fetching connectors ${err.message}`);
+        })
     }
 
     render() {
         return (
             <div className={css.container}>
                 <NewConnectorModal />
-                <KafkaConnectors connectors={this.state.connectors} />
+                <KafkaConnectors connectors={this.state.connectors} refresh={this.getConnectors}/>
             </div>
         );
     }
--- packages/splunk-kafka-connect/src/components/KafkaConnectors.jsx
+++ packages/splunk-kafka-connect/src/components/KafkaConnectors.jsx
@@ -1,32 +1,113 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Table from '@splunk/react-ui/Table';
+import Pause from '@splunk/react-icons/Pause';
+import Refresh from '@splunk/react-icons/Refresh';
+import Play from '@splunk/react-icons/Play';
+import Trash from '@splunk/react-icons/Trash';
+import Button from '@splunk/react-ui/Button';
+import Tooltip from '@splunk/react-ui/Tooltip';
+import urls from '../urls';
 import css from './KafkaConnectors.css';
 
 class KafkaConnectors extends Component {
     static propTypes = {
         connectors: PropTypes.array,
+        refresh: PropTypes.func
     };
 
     handlerClick = (e, data) => {
         window.location.href = `#/connector/${data}`
     };
 
+    handlePause = (e, {value}) => {
+        const url = `${urls.baseUrl}${urls.connectors}/${value}/pause`
+        fetch(url, {
+            method: 'put'
+        }).then(res => {
+            if (res.ok) {
+                this.props.refresh();
+            }
+        }).catch(error => {
+            console.error(error.message);
+        })
+    };
+
+    handleResume = (e, {value}) => {
+        const url = `${urls.baseUrl}${urls.connectors}/${value}/resume`
+        fetch(url, {
+            method: 'put'
+        }).then(res => {
+            if (res.ok) {
+                this.props.refresh();
+            }
+        }).catch(error => {
+            console.error(error.message);
+        })
+    };
+
+    handleRestart = (e, {value}) => {
+        const url = `${urls.baseUrl}${urls.connectors}/${value}/restart`
+        fetch(url, {
+            method: 'post'
+        }).then(res => {
+            if (res.ok) {
+                this.props.refresh();
+            }
+        }).catch(error => {
+            console.error(error.message);
+        })
+    };
+
+    handleDelete = (e, {value}) => {
+        const url = `${urls.baseUrl}${urls.connectors}/${value}`
+        fetch(url, {
+            method: 'delete'
+        }).then(res => {
+            if (res.ok) {
+                this.props.refresh();
+            }
+        }).catch(error => {
+            console.error(error.message);
+        })
+    };
+
     render() {
+        const tooltipStyle = {
+            marginRight: '5px'
+        }
         return (
             <div className={css.container}>
                 <Table stripeRows>
                     <Table.Head>
                         <Table.HeadCell>Name</Table.HeadCell>
-                        {<Table.HeadCell>Status</Table.HeadCell>}
-                        {<Table.HeadCell>Connection</Table.HeadCell>}
+                        <Table.HeadCell>Connection</Table.HeadCell>
+                        <Table.HeadCell>Status</Table.HeadCell>
+                        <Table.HeadCell>Actions</Table.HeadCell>
                     </Table.Head>
                     <Table.Body>
                         {this.props.connectors.map(row => (
                             <Table.Row key={row.name}>
                                 <Table.Cell onClick={this.handlerClick} data={row.name}>{row.name}</Table.Cell>
-                                <Table.Cell>{row.status}</Table.Cell>
                                 <Table.Cell>{row.numTasks}</Table.Cell>
+                                <Table.Cell>{row.status}</Table.Cell>
+                                <Table.Cell>
+                                    {
+                                        row.status === 'RUNNING'?
+                                        <Tooltip style={tooltipStyle} content="Pause">
+                                            <Button size="small" value={row.name} icon={<Pause />} onClick={this.handlePause}/>
+                                        </Tooltip> :
+                                        <Tooltip style={tooltipStyle} content="Resume">
+                                            <Button size="small" value={row.name} icon={<Play />} onClick={this.handleResume}/>
+                                        </Tooltip>
+                                    }
+                                    <Tooltip style={tooltipStyle} content="Restart">
+                                        <Button size="small" value={row.name} icon={<Refresh />} onClick={this.handleRestart} />
+                                    </Tooltip>
+                                    <Tooltip style={tooltipStyle} content="Delete">
+                                        <Button error size="small" value={row.name} icon={<Trash />} onClick={this.handleDelete}/>
+                                    </Tooltip>
+                                </Table.Cell>
                             </Table.Row>
                         ))}
                     </Table.Body>
