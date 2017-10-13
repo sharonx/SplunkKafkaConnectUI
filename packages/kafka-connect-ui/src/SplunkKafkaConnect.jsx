a2f6e566953d23474477727e7d57fa94333f177e
Sharon Xie <rxie@splunk.com>
2017-10-12 17:14:29 -0700

    re-query server when backend is rebalancing

--- packages/kafka-connect-ui/src/SplunkKafkaConnect.jsx
+++ packages/kafka-connect-ui/src/SplunkKafkaConnect.jsx
@@ -10,7 +10,7 @@ class SplunkKafkaConnect extends Component {
     constructor(props) {
         super(props);
         this.state = { connectors: [], open: false, plugins: [] };
-        
+
         this.getConnectors();
         const connectorPluginsUrl = urls.baseUrl + urls.connectorPlugins;
 
@@ -28,76 +28,92 @@ class SplunkKafkaConnect extends Component {
             });
     }
 
+    
     getConnectors = () => {
         const connectors = [];
         const connectorsUrl = urls.baseUrl + urls.connectors;
         fetch(connectorsUrl)
-        .then(res => {
-            if (res.ok) {
-                return res.json();
-            }
-
-            throw new Error('Cannot fetch connectors');
-        }).then(res => {
-            const promises = [];
-
-            _.each(res, (connector) => {
-                promises.push(fetch(`${connectorsUrl}/${connector}`));
-            })
-
-            return Promise.all(promises);
-        }).then(promises => {
-            const resArray = [];
-            _.each(promises, promise=> {
-                resArray.push(promise.json());
-            });
-
-            return Promise.all(resArray);
-        }).then(jsonArray => {
-            const statusPromises = [];
-            _.each(jsonArray, (connector) => {
-                statusPromises.push(fetch(`${connectorsUrl}/${connector.name}/status`));
-            })
-
-            _.each(jsonArray, data => {
-                const connector = {};
-                connector.name = data.name;
-                connector.numTasks = data.tasks.length;
-                connector.class = data.config['connector.class'];
-                connectors.push(connector);
-            });
-
-            return Promise.all(statusPromises);
-
-            // this.setState({connectors});
-        }).then(promises => {
-            const resArray = [];
-            _.each(promises, promise=> {
-                resArray.push(promise.json());
-            });
-
-            return Promise.all(resArray);
-        }).then(jsonStatusArray => {
-
-            _.each(connectors, connector => {
-                const find = _.find(jsonStatusArray, connectorStatus => connector.name === connectorStatus.name);
-                if (find) {
-                    connector.status = find.connector.state;
+            .then(res => {
+                if (res.ok) {
+                    return res.json();
                 }
-            });
 
-            this.setState({connectors});
-        }).catch(err => {
-            console.error(`Problems with fetching connectors ${err.message}`);
-        })
+                if (res.status === 409) {
+                    return this.sleep(1000).then(() =>  fetch(connectorsUrl))
+                        .then(secondRes => {
+                            if (secondRes.ok) {
+                                return secondRes.json();
+                            }
+
+                            throw new Error('Cannot fetch connectors');
+                        })
+                }
+                throw new Error('Cannot fetch connectors');
+            }).then(res => {
+                const promises = [];
+
+                _.each(res, (connector) => {
+                    promises.push(fetch(`${connectorsUrl}/${connector}`));
+                })
+
+                return Promise.all(promises);
+            }).then(promises => {
+                const resArray = [];
+                _.each(promises, promise => {
+                    resArray.push(promise.json());
+                });
+
+                return Promise.all(resArray);
+            }).then(jsonArray => {
+                const statusPromises = [];
+                _.each(jsonArray, (connector) => {
+                    statusPromises.push(fetch(`${connectorsUrl}/${connector.name}/status`));
+                })
+
+                _.each(jsonArray, data => {
+                    const connector = {};
+                    connector.name = data.name;
+                    connector.numTasks = data.tasks.length;
+                    connector.class = data.config['connector.class'];
+                    connectors.push(connector);
+                });
+
+                return Promise.all(statusPromises);
+
+                // this.setState({connectors});
+            }).then(promises => {
+                const resArray = [];
+                _.each(promises, promise => {
+                    resArray.push(promise.json());
+                });
+
+                return Promise.all(resArray);
+            }).then(jsonStatusArray => {
+
+                _.each(connectors, connector => {
+                    const find = _.find(jsonStatusArray, connectorStatus => connector.name === connectorStatus.name);
+                    if (find) {
+                        connector.status = find.connector.state;
+                    }
+                });
+
+                this.setState({ connectors });
+            }).catch(err => {
+                console.error(`Problems with fetching connectors ${err.message}`);
+            })
     }
 
+    sleep = (ms) => {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
+      
+    
     render() {
         return (
             <div className={css.container}>
                 <h2>Kafka Connectors</h2>
-                <NewConnectorModal refresh={this.getConnectors} plugins={this.state.plugins}/>
-                <KafkaConnectors connectors={this.state.connectors} refresh={this.getConnectors}/>
+                <NewConnectorModal refresh={this.getConnectors} plugins={this.state.plugins} />
+                <KafkaConnectors connectors={this.state.connectors} refresh={this.getConnectors} />
                 <TopologyGraph plugins={this.state.plugins} connectors={this.state.connectors} />
             </div>
         );
